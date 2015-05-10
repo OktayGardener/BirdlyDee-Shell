0df0a68be23f9f56af74ff4223ce97a4060520aa
Oktay Gardener <oktayb@kth.se>
2015-05-10 15:33:54 +0200

    no more seg fault

--- cd.c
+++ cd.c
@@ -1,12 +1,22 @@
 #include "cd.h"
 #include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
 
 void changedir(char *args){
     char *cmd;
     printf("%s\n", args);
     /* get argument after cd */
     cmd = strtok(args, "cd ");
-    if(strcmp(cmd, "~") == 0) {
+    printf("%s\n", cmd);
+
+    if(cmd == NULL){
+        perror("Command failed");
+    }
+    else if(strcmp(cmd, "~") == 0) {
         chdir(getenv("HOME"));
     } else {
         chdir(cmd);
