ba144e76a436b4bf3508136beac5b819373d9997
Oktay Gardener <oktayb@kth.se>
2015-05-11 13:30:01 +0200

    pipes.c

--- pipes.c
+++ pipes.c
@@ -1,78 +1,41 @@
+#include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
 
-struct command
+int main(void)
 {
-  const char **argv;
-};
+        int     fd[2], nbytes;
+        pid_t   childpid;
+        char    string[] = "ls";
+        char    readbuffer[80];
 
-int
-spawn_proc (int in, int out, struct command *cmd)
-{
-  pid_t pid;
+        pipe(fd);
 
-  if ((pid = fork ()) == 0)
-    {
-      if (in != 0)
+        if((childpid = fork()) == -1)
         {
-          dup2 (in, 0);
-          close (in);
+                perror("fork");
+                exit(1);
         }
 
-      if (out != 1)
+        if(childpid == 0)
         {
-          dup2 (out, 1);
-          close (out);
+                /* Child process closes up input side of pipe */
+                close(fd[0]);
+
+                /* Send "string" through the output side of pipe */
+                write(fd[1], string, (strlen(string)+1));
+                exit(0);
+        }
+        else
+        {
+                /* Parent process closes up output side of pipe */
+                close(fd[1]);
+
+                /* Read in a string from the pipe */
+                nbytes = read(fd[0], readbuffer, sizeof(readbuffer));
+                printf("Received string: %s", readbuffer);
         }
 
-      return execvp (cmd->argv [0], (char * const *)cmd->argv);
-    }
-
-  return pid;
-}
-
-int
-fork_pipes (int n, struct command *cmd)
-{
-  int i;
-  pid_t pid;
-  int in, fd [2];
-
-  /* The first process should get its input from the original file descriptor 0.  */
-  in = 0;
-
-  /* Note the loop bound, we spawn here all, but the last stage of the pipeline.  */
-  for (i = 0; i < n - 1; ++i)
-    {
-      pipe (fd);
-
-      /* f [1] is the write end of the pipe, we carry `in` from the prev iteration.  */
-      spawn_proc (in, fd [1], cmd + i);
-
-      /* No need for the write and of the pipe, the child will write here.  */
-      close (fd [1]);
-
-      /* Keep the read end of the pipe, the next child will read from there.  */
-      in = fd [0];
-    }
-
-  /* Last stage of the pipeline - set stdin be the read end of the previous pipe
-     and output to the original file descriptor 1. */
-  if (in != 0)
-    dup2 (in, 0);
-
-  /* Execute the last stage with the current process. */
-  return execvp (cmd [i].argv [0], (char * const *)cmd [i].argv);
-}
-
-int
-main ()
-{
-  const char *ls[] = { "ls", "-l", 0 };
-  const char *awk[] = { "awk", "{print $1}", 0 };
-  const char *sort[] = { "sort", 0 };
-  const char *uniq[] = { "uniq", 0 };
-
-  struct command cmd [] = { {ls}, {sort}};
-
-  return fork_pipes (2, cmd);
+        return(0);
 }
